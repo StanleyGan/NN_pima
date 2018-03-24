63f76e716ebebde4c378f35502dc2b2989c8b102
StanleyGan <glgan@sfu.ca>
2018-03-24 14:50:44 -0700

    Fixed bugs and had an MLP object working

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 import random
+import itertools
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.style.use('ggplot')
 
 class MLP():
     def __init__(self):
@@ -16,46 +20,31 @@ class MLP():
         self.dropout_list_placeholder = None
         self.y_pred = None
 
-    def setNeurons(n):
-        self._neurons = n
-        self._numOfLayers = len(n)
+    def _setWeights(self,shape):
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
-    def returnNumOfLayers():
-        return self._numOfLayers
-
-    def setAct(a):
-        self._activations = a
-
-    def setDropout(p):
-        self._dropout = p
-
-    def setInputOutputDim(i, o):
-        self._inputDim = i
-        self._outputDim = o
-
-    def _setWeights(shape):
-        return tf.Variable(tf.truncated_normal(shape, stdev=0.1))
-
-    def _setBias(shape):
-        return tf.Variable(tf.truncated_normal(shape, stdev=0.1))
+    def _setBias(self,shape):
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
     def _setVariables(self):
-        numOfLayers = self._numOfLayers
+        numOfLayers = self.returnNumOfLayers()
         neurons = self._neurons
+        inputDim = self._inputDim
 
-        for i in range(len(numOfLayers)):
-            self._variables["W{}".format(i+1)] = self._setWeights([self._inputDim, neurons[i]])
+        for i in range(numOfLayers):
+            self._variables["W{}".format(i+1)] = self._setWeights([inputDim, neurons[i]])
             self._variables["b{}".format(i+1)] = self._setBias([neurons[i]])
+            inputDim = neurons[i]
 
-        self._variables["W{}".format(i+1)] = self._setWeights([neurons[i], self._outputDim])
-        self._variables["b{}".format(i+1)] = self._setBias([self._outputDim])
+        self._variables["W{}".format(numOfLayers+1)] = self._setWeights([neurons[numOfLayers-1], self._outputDim])
+        self._variables["b{}".format(numOfLayers+1)] = self._setBias([self._outputDim])
 
     def _setPlaceholders(self):
-        self.X_placeholder =  tf.placeholder(tf.float64, shape=[None, self._inputDim])
-        self.y_placeholder =  tf.placeholder(tf.float64, shape=[None, self._outputDim])
-        self.dropout_list_placeholder = [tf.placeholder(tf.float64)]*len(self._dropout)
+        self.X_placeholder =  tf.placeholder(tf.float32, shape=[None, self._inputDim])
+        self.y_placeholder =  tf.placeholder(tf.float32, shape=[None, self._outputDim])
+        self.dropout_list_placeholder = [tf.placeholder(tf.float32) for _ in xrange(len(self._dropout))]
 
-    def _activations(a, input):
+    def _applyActivations(self,a, input):
         if a == "elu":
             return tf.nn.elu(input)
         elif a == "sigmoid":
@@ -74,18 +63,36 @@ class MLP():
         self._setPlaceholders()
         input = self.X_placeholder
 
-        for i in range(self._numOfLayers):
-            input = tf.add( tf.matmul(input, self._variables["W{}".format(i)]), self._variables["b{}".format(i)] )
-            input = self._activations(self._activations[i], input)
+        for i in range(self.returnNumOfLayers()):
+            input = tf.add( tf.matmul(input, self._variables["W{}".format(i+1)]), self._variables["b{}".format(i+1)] )
+            input = self._applyActivations(self._activations[i], input)
             input = tf.nn.dropout(input, keep_prob=self.dropout_list_placeholder[i])
 
+        input = tf.add( tf.matmul(input, self._variables["W{}".format(self.returnNumOfLayers()+1)]), self._variables["b{}".format(self.returnNumOfLayers()+1)] )
         self.y_pred = input
 
-    def shuffledIndices(total_samples, seed):
+    def setNeurons(self,n):
+        self._neurons = n
+        self._numOfLayers = len(n)
+
+    def returnNumOfLayers(self):
+        return self._numOfLayers
+
+    def setAct(self,a):
+        self._activations = a
+
+    def setDropout(self,p):
+        self._dropout = p
+
+    def setInputOutputDim(self,i, o):
+        self._inputDim = i
+        self._outputDim = o
+
+    def shuffledIndices(self,total_samples, seed):
         random.seed(seed)
         return random.sample(range(total_samples), total_samples)
 
-    def buildModel( neurons, activations, dropout, inputDim, outputDim):
+    def buildModel(self, neurons, activations, dropout, inputDim, outputDim):
         assert(len(neurons) == len(dropout)), "Length of neurons and dropout lists are different!"
         assert(len(activations) == len(neurons)), "Length of activations and neurons list are different!"
 
@@ -97,13 +104,19 @@ class MLP():
         self._setVariables()
         self._buildCompGraph()
 
-    def train(X, y, X_test, y_test, lr=1e-3, num_epochs=100, batch_size=128, seed=1):
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.y_pred, labels=self.y_placeholder))
-        accuracy = tf.reduce_mean( tf.equal( tf.argmax(self.y_pred,1), tf.argmax(self.y_placeholder,1)) )
+    def train(self, X, y, X_test, y_test, lr=1e-3, num_epochs=100, batch_size=128, seed=1):
+        # X = tf.cast(X, tf.float32)
+        # y = tf.cast(y, tf.float32)
+        # X_test = tf.cast(X_test, tf.float32)
+        # y_test = tf.cast(y_test, tf.float32)
+
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.y_pred, labels=self.y_placeholder))
+        accuracy = tf.reduce_mean( tf.cast(tf.equal( tf.argmax(self.y_pred,1), tf.argmax(self.y_placeholder,1)), tf.float32) )
         optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(cost)
         total_samples = int(X.shape[0])
 
         init = tf.global_variables_initializer()
+        training_loss = list()
 
         with tf.Session() as sess:
             sess.run(init)
@@ -113,25 +126,42 @@ class MLP():
                 train_index = 0
 
                 while train_index+batch_size <= total_samples:
-                    X_batch = X[train_index:train_index+batch_size]
-                    y_batch = y[train_index:train_index+batch_size]
-                    feed_dict_train = {self.X_placeholder:X_batch, self.y_placeholder:y_batch, self.dropout_list_placeholder:self._dropout}
+                    X_batch = X[train_index:train_index+batch_size,:]
+                    y_batch = y[train_index:train_index+batch_size,:]
+                    feed_dict_train = {ph:do for (ph, do) in itertools.izip(self.dropout_list_placeholder, self._dropout)}
+                    feed_dict_train[self.X_placeholder] = X_batch
+                    feed_dict_train[self.y_placeholder] = y_batch
                     sess.run(optimizer, feed_dict=feed_dict_train)
                     train_index += batch_size
 
                 if train_index < total_samples:
-                    X_batch = X[train_index:]
-                    y_batch = y[train_index:]
-                    feed_dict_train = {self.X_placeholder:X_batch, self.y_placeholder:y_batch, self.dropout_list_placeholder:self._dropout}
+                    X_batch = X[train_index:,:]
+                    y_batch = y[train_index:,:]
+                    feed_dict_train = {ph:do for (ph, do) in itertools.izip(self.dropout_list_placeholder, self._dropout)}
+                    feed_dict_train[self.X_placeholder] = X_batch
+                    feed_dict_train[self.y_placeholder] = y_batch
                     sess.run(optimizer, feed_dict=feed_dict_train)
 
-                train_cost = sess.run(cost, feed_dict={self.X_placeholder:X, self.y_placeholder:y, self.dropout_list_placeholder:self._dropout})
-                test_acc = sess.run(accuracy, feed_dict={self.X_placeholder:X_test, self.y_placeholder:y_test, self.dropout_list_placeholder:[1]*self.returnNumOfLayers()} )
+                feed_dict_train = {ph:do for (ph, do) in itertools.izip(self.dropout_list_placeholder, self._dropout)}
+                feed_dict_train[self.X_placeholder] = X_batch
+                feed_dict_train[self.y_placeholder] = y_batch
+                train_cost = sess.run(cost, feed_dict=feed_dict_train)
+                training_loss.append(train_cost)
 
-                print("Epoch {0} : Training loss: {1}, \t test accuracy : {2}".format(epoch, train_cost, test_acc))
+                feed_dict_test = {ph:1.0 for ph in self.dropout_list_placeholder}
+                feed_dict_test[self.X_placeholder] = X_test
+                feed_dict_test[self.y_placeholder] = y_test
+                test_acc = sess.run(accuracy, feed_dict=feed_dict_test )
+
+                if epoch % 10 == 0:
+                    print("Epoch {0} : Training loss: {1}, \n test accuracy : {2}\n".format(epoch+1, train_cost, test_acc))
                 seed += 1
 
-    def predict(X):
+            #Plot training loss graph
+            plt.plot(training_loss)
+            plt.show()
+
+    def predict(self,X):
         sess = tf.Session()
         y_pred = sess.run(self.y_pred, feed_dict={X:X, self.dropout_list_placeholder:[1]*self.returnNumOfLayers()})
 
